64d7b2fbfe7460ce6d89305d4e347e26be973ab7
Abhimanyu Kumar <abhimanyuku024@gmail.com>
2026-01-06 15:55:07 -0800

    student sign up and log in base created, validation of input remaining

--- server/index.ts
+++ server/index.ts
@@ -1,7 +1,12 @@
 import express from 'express';
 import jwt from 'jsonwebtoken';
 import cors from 'cors';
+import bcrypt from 'bcrypt'
 import { authMiddleware } from './auth.js';
+import { UserModel, AdminModel, CourseModel } from './db.js';
+
+const JWT_SECRET = process.env.JWT_SECRET
+if (!JWT_SECRET) throw new Error("JWT_SECRET missing");
 
 const app = express();
 
@@ -11,13 +16,67 @@ app.use(express.json()); // convert body to json
 // LOGIN/SIGNUP -----------
 
 // sign up student - ideally authenticate student's existence before signing up
-app.post('/api/v1/signup', (req, res) => {
+app.post('/api/v1/signup', async (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+    const studentId = req.body.studentId;
+
+    // validate email, password and studentId, using zod
+    
+    const hashedPass = await bcrypt.hash(password, 10);
+    
+    try{
+        await UserModel.create({
+            email,
+            password: hashedPass,
+            studentId
+        })
+
+        res.status(201).json({
+            message: "User created successfully"
+        })
+        return;
+    } catch(err){
+        res.status(400).json({
+            message: "User could not be created\n" + err
+        })
+        return;
+    }
 
 })
 
 // log in student
-app.post('/api/v1/login', (req, res) => {
+app.post('/api/v1/login', async (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
 
+    try{
+        const userFound = await UserModel.findOne({
+            email
+        })
+
+        if(!userFound){
+            return res.status(400).json({
+                message: "User not found via email"
+            })
+        }
+
+        const isValid = await bcrypt.compare(password, userFound.password)
+
+        if(isValid){
+            const token = jwt.sign({id: userFound._id}, JWT_SECRET)
+            res.status(201).json({
+                message: "User signed in successfully",
+                token: token
+            })
+            return;
+        }
+    } catch(err){
+        res.status(400).json({
+            message: "User could not be created\n" + err
+        })
+        return;
+    }
 })
 
 // log in admin (no sign up admin endpoint - design sign up later)
