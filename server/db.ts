0f8700324fc546dafdfd31816565b76a6a413511
Abhimanyu Kumar <abhimanyuku024@gmail.com>
2026-01-06 10:54:29 -0800

    set up backend skeleton, typescript setup

---
+++ server/db.ts
@@ -0,0 +1,4 @@
+import mongoose, {Schema, model} from 'mongoose'
+
+// mongoose.connect("") - connect to db, later
+
--- server/index.ts
+++ server/index.ts
@@ -1,7 +1,69 @@
 import express from 'express';
+import jwt from 'jsonwebtoken';
+import cors from 'cors';
 
 const app = express();
 
+app.use(express.json()); // convert body to json
+// app.use(cors()); - enable later, while connecting frontend
+
+// LOGIN/SIGNUP -----------
+
+// sign up student - ideally authenticate student's existence before signing up
+app.post('/api/v1/signup', (req, res) => {
+
+})
+
+// log in student
+app.post('/api/v1/login', (req, res) => {
+
+})
+
+// log in admin (no sign up admin endpoint - design sign up later)
+app.post('/api/v1/admin/login', (req, res) => {
+
+})
+
+// STUDENT COURSE ENDPOINTS -----------
+
+// view all courses - public
+app.get('/api/v1/courses', (req, res) => {
+
+})
+
+// view a specific course information - public
+app.get('/api/v1/courses/:course', (req, res) => {
+
+})
+
+// register for a specific course - authenticated
+app.post('/api/v1/courses/:course', (req, res) => {
+
+})
+
+// unregister from a course - authenticated
+app.post('/api/v1/courses/:course', (req, res) => {
+
+})
+
+// ADMIN COURSE ENDPOINTS -----------
+
+// admin: post a new course - admin authenticated
+app.post('/api/v1/admin/courses', (req, res) => {
+
+})
+
+// admin: update an existing course
+app.put('/api/v1/admin/courses/:course', (req, res) => {
+
+})
+
+// admin: delete an existing course
+app.delete('/api/v1/admin/courses/:course', (req, res) => {
+
+})
+
+// start server
 app.listen(3000, () => {
     console.log("Server started on PORT 3000!")
 })
