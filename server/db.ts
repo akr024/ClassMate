f001f2a8dddd7e5f57b0eddab04ea39e0ad27937
Abhimanyu Kumar <abhimanyuku024@gmail.com>
2026-01-06 15:09:05 -0800

    develop schemas for users, admins and courses

--- server/db.ts
+++ server/db.ts
@@ -2,3 +2,30 @@ import mongoose, {Schema, model} from 'mongoose'
 
 // mongoose.connect("") - connect to db, later
 
+const userSchema = new Schema({
+    email: {type: String, unique: true, required: true}, // validate its ends with "@northeastern.edu"
+    password: {type: String, required: true},
+    studentId: {type: Number, required: true, unique: true}, // ideally this student ID would be verified to make sure the student actually exists
+    // type: {type: String, enum: ["Undergraduate", "Graduate"]} - later feature
+})
+
+export const UserModel = model("User", userSchema);
+
+// only an existing admin can add another admin - for now, this is how authentication of admins is done
+const adminSchema = new Schema({
+    email: {type: String, unique: true, required: true},
+    password: {type: String, required: true},
+    adminId: {type: Number, required: true, unique: true}
+})
+
+export const AdminModel = model("Admin", adminSchema);
+
+const courseSchema = new Schema({
+    courseName: {type: String, required: true},
+    courseId: {type: String, required: true, unique: true},
+    admin: {type: mongoose.Types.ObjectId, ref: "Admin"}, // admin = professor = instructor
+    description: String,
+    seats: Number, // how many seats are available in this course
+})
+
+export const CourseModel = model("Course", courseSchema);
