c49d1c5115692a261fce5bcac3ce8727a021ff8c
Abhimanyu Kumar <abhimanyuku024@gmail.com>
2026-01-27 23:45:28 -0800

    add shadcn installations, generic api fetch

---
+++ client/lib/api.ts
@@ -0,0 +1,21 @@
+const API_URL = process.env.NEXT_PUBLIC_API_URL!;
+
+export async function apiFetch<T>(
+  path: string,
+  options: RequestInit = {}
+): Promise<T> {
+  const res = await fetch(`${API_URL}${path}`, {
+    credentials: "include",
+    headers: {
+      "Content-Type": "application/json",
+      ...options.headers,
+    },
+    ...options,
+  });
+
+  if (!res.ok) {
+    throw new Error(await res.text());
+  }
+
+  return res.json();
+}
