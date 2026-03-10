e47aa356845f2431240e0c9a02d290b0235e7410
Abhimanyu Kumar <abhimanyuku024@gmail.com>
2026-03-09 17:19:27 -0700

    add logic for getting courses and sections

--- services/course-service/src/controllers/courseController.js
+++ services/course-service/src/controllers/courseController.js
@@ -13,5 +13,41 @@ export async function createCourse(req, res){
         `, [id, courseCode, name, description] // parameterized sql, avoids sql injections
     )
 
-    res.send(result.rows[0])
+    return res.send(result.rows[0])
+}
+
+export async function createSection(req, res){
+    const { course_id, section_number, capacity } = req.body;
+    const id = uuidv4();
+
+    const result = await pool.query(
+        `
+        INSERT INTO courses (id, courseCode, description)
+        VALUES ($1, $2, $3, $4)
+        RETURNING *
+        `, [id, course_id, section_number, capacity] // parameterized sql, avoids sql injections
+    )
+
+    return res.send(result.rows[0])
+}
+
+export async function getCourses(req, res){
+    const result = await pool.query(
+        `
+        SELECT * FROM courses
+        ORDER BY created_at DESC
+        `
+    )
+
+    return res.send(result.rows)
+}
+
+export async function getSections(req, res){
+    const result = await pool.query(
+        `
+        SELECT * FROM sections
+        `
+    )
+
+    return res.send(result.rows)
 }
