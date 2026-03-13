364bbb46a08f16096ee43e4e72a9427a33b2f2fd
Abhimanyu Kumar <abhimanyuku024@gmail.com>
2026-03-13 11:08:15 -0700

    add database schema and migrations for enrollment service

---
+++ services/enrollment-service/migrations/1773424522355_init-enrollment-tables.js
@@ -0,0 +1,62 @@
+export const shorthands = undefined;
+
+export const up = (pgm) => {
+    pgm.createTable("sections", {
+        id: {
+            type: "uuid",
+            primaryKey: true
+        },
+        course_id: {
+            type: "uuid",
+            notNull: true,
+            references: "courses",
+            onDelete: "cascade"
+        },
+        section_number: {
+            type: "integer",
+            notNull: true
+        },
+        capacity: {
+            type: "integer",
+            notNull: true
+        },
+        seats_remaning: {
+            type: "integer",
+            notNull: true
+        },
+        created_at: {
+            type: "timestamp",
+            default: pgm.func("current_timestamp")
+        }
+    })
+
+    pgm.createTable("enrollments", {
+        id: {
+            type: "uuid",
+            primaryKey: true
+        },
+        student_id: {
+            type: "uuid",
+            notNull: true
+        },
+        section_id: {
+            type: "uuid",
+            notNull: true
+        },
+        created_at: {
+            type: "timestamp",
+            default: pgm.func("current_timestamp")
+        }
+    })
+
+    pgm.addConstraint(
+        "enrollments",
+        "unique_student_section",
+        "UNIQUE(student_id, section_id)"
+    )
+};
+
+export const down = (pgm) => {
+    pgm.dropTable("sections");
+    pgm.dropTable("enrollments")
+};
