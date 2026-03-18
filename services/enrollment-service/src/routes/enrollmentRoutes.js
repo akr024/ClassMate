4440016be5ef310b11245d488d908298199facd1
Abhimanyu Kumar <abhimanyuku024@gmail.com>
2026-03-17 22:05:37 -0700

    adjust router for deenroll student endpoint

--- services/enrollment-service/src/routes/enrollmentRoutes.js
+++ services/enrollment-service/src/routes/enrollmentRoutes.js
@@ -1,5 +1,9 @@
-import { enrollStudent } from "../controllers/enrollmentController.js";
+import { deEnrollStudent, enrollStudent } from "../controllers/enrollmentController.js";
 
 export async function enrollmentRoutes(fastify){
     fastify.post("/enroll", enrollStudent)
+}
+
+export async function enrollmentRoutes(fastify){
+    fastify.post("/deenroll", deEnrollStudent)
 }
