d3a149a71cf3dc7f309310725e2f2e1fd5fa6a9b
Abhimanyu Kumar <abhimanyuku024@gmail.com>
2026-04-28 15:03:38 -0700

    bug fix: add result variable

--- services/enrollment-service/src/events/handlers/sectionCreated.js
+++ services/enrollment-service/src/events/handlers/sectionCreated.js
@@ -4,7 +4,7 @@ export async function handleSectionCreated(event){
     console.log("Received section.created event!", event)
     const {sectionId, courseId, sectionNumber, capacity} = event
     try {
-        await pool.query(
+        const result = await pool.query(
             `
             INSERT INTO sections (id, course_id, section_number, capacity, seats_remaining)
             VALUES ($1, $2, $3, $4, $4)
